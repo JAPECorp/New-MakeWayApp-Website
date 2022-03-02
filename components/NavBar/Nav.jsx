9c972a96aa597687c70fc2531464de30894d67e3
Eugene Debrah <debraheug@gmail.com>
2022-03-02 13:45:35 +0000

    NavBar

---
+++ components/NavBar/Nav.jsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+import Link from "next/link";
+import { IoClose, IoMenu } from "react-icons/io5";
+
+const NavItem = (props) => (
+  <li>
+    <Link href={props.href}>
+      <a className="nav-link">{props.text}</a>
+    </Link>
+  </li>
+);
+
+function Nav(props) {
+  return (
+    <nav>
+      <div className="h-12 w-32 relative">
+        <Image
+          layout="fill"
+          objectFit="contain"
+          priority
+          src={"/Logo.png"}
+          alt="makeway logo"
+        />
+      </div>
+
+      <div className="hidden md:block">
+        <ul className="inline-flex space md:gap-5 text-base font-medium">
+          <NavItem href="/about-us" text="How It Works" />
+          <NavItem href="/services" text="FAQ's" />
+          <NavItem href="/portfolio" text="About" />
+        </ul>
+      </div>
+
+      <div className="block md:hidden ">
+        <div
+          className="inline-flex items-center justify-center p-2 text-black"
+          aria-controls="mobile-menu"
+          aria-expanded="false"
+        >
+          <span className="sr-only">Open Menu</span>
+          <span
+            className="cursor-pointer"
+            onClick={() => props.setSideBar((isOpen) => !isOpen)}
+          >
+            {!props.sideBar ? <IoMenu size={35} /> : <IoClose size={35} />}
+          </span>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,91 @@
+const { screens } = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      xxxsm: "200px",
+      xxsm: "340px",
+      xsm: "480px",
+      ...screens,
+    },
+
+    extend: {
+      colors: {
+        "makeway-dark": "#2A2E43",
+        "makeway-gray-dark": "#0000000F",
+        "makeway-gray": "#7A7A7A",
+        "makeway-red": "#F40E41",
+        "makeway-blue-deep": "#1438C9",
+        "makeway-blue-dark": "#011F3D",
+        "makeway-blue-light": "#1AA1DC",
+      },
+
+      fontSize: {
+        "makeway-xxxlg": "72px",
+        "makeway-xxlg": "62px",
+        "makeway-xlg": "52px",
+        "makeway-lg": "44px",
+
+        "makeway-xxmd": "38px",
+        "makeway-xmd": "32px",
+        "makeway-md": "28px",
+
+        "makeway-sm-md": "24px",
+
+        "makeway-sm": "16px",
+        "makeway-xsm": "14px",
+        "makeway-xxsm": "10px",
+      },
+
+      minHeight: (theme) => ({
+        "h-min-screen": "100vh",
+        "makeway-px-450": "450px",
+        ...theme("spacing"),
+      }),
+
+      minWidth: (theme) => ({
+        ...theme("spacing"),
+      }),
+
+      height: {
+        "makeway-h-nav": "10%",
+        "makeway-h-hero": "85%",
+        "5/2": "40%",
+        "makeway-px-80": "80px",
+        "makeway-px-100": "100px",
+        "makeway-px-120": "120px",
+        "makeway-px-150": "150px",
+        "makeway-px-40": "40px",
+        "makeway-px-50": "50px",
+        "makeway-px-450": "450px",
+      },
+
+      inset: {
+        "makeway-hero-offset-t": "15%",
+      },
+
+      backgroundImage: {
+        "makeway-hero-gradient":
+          "linear-gradient(0deg, #8F1742 10.84%, #2A2E43 93.27%)",
+      },
+
+      width: {
+        "makeway-px-270": "270px",
+        "makeway-px-124": "124px",
+      },
+
+      borderRadius: {
+        "makeway-px-20": "20px",
+      },
+
+      fontFamily: {
+        "makeway-montserrat": ["Montserrat"],
+      },
+    },
   },
   plugins: [],
-}
+};
