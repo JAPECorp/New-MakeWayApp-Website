2e0aff3979721871e2bf88e59caae2a7d527d8a2
Eugene Debrah <debraheug@gmail.com>
2022-03-04 12:50:46 +0000

    Motion on Hero Section

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import AndroidButton from "../utilities/Buttons/AndroidButton";
 import AppleButton from "../utilities/Buttons/AppleButton";
+import { motion } from "framer-motion";
 
 function Hero() {
   return (
     <section className="relative top-makeway-hero-offset-t">
       <div className="container flex text-center md:text-left lg:pl-32 mx-auto">
-        <h2 className="leading-tight text-black text-left lg:mt-32 text-7xl md:text-4 lg:text-8xl lg:text-left mb-6">
+        <motion.h2
+          initial="hidden"
+          animate="visible"
+          variants={{
+            hidden: {
+              scale: 0.8,
+              opacity: 0,
+            },
+            visible: {
+              scale: 1,
+              opacity: 1,
+              transition: {
+                delay: 0.5,
+              },
+            },
+          }}
+          className="leading-tight text-black text-left lg:mt-32 text-7xl md:text-4 lg:text-8xl lg:text-left mb-6"
+        >
           No More Delay
           <p className="mt-6 text-lg font-normal text-left text-gray-500">
             Get your Car moving quickly whenever, wherever at anytime with just
@@ -17,13 +35,28 @@ function Hero() {
             <AndroidButton />
             <AppleButton />
           </div>
-        </h2>
-        <div className="md:bg-phone" />
+        </motion.h2>
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={{
+            hidden: {
+              scale: 1.3,
+              opacity: 0,
+            },
+            visible: {
+              scale: 1,
+              opacity: 1,
+              transition: {
+                delay: 0.7,
+              },
+            },
+          }}
+          className="md:bg-phone"
+        />
       </div>
     </section>
   );
 }
 
 export default Hero;
-
-// container flex flex-col-reverse lg:flex-row items-center gap-12 mt-14 lg:mt-28
