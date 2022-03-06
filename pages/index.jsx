593182977f97f13e52a5d8f0b5f306dc91c1f91b
Eugene Debrah <debraheug@gmail.com>
2022-03-06 19:12:10 +0000

    sideBar..

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,26 +3,33 @@ import Hero from "../components/Hero/Hero";
 import Nav from "../components/NavBar/Nav";
 import Service from "../components/Service /Service";
 import How from "../components/HowItWorks/How";
+import SideBar from "../components/NavBar/sidebar";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>MakeWay App Website</title>
-        <meta name="description" content="Developed by JAPECorp" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  const [sideBar, setSideBar] = useState(false);
 
-      <section className="h-screen">
-        <Nav />
-        <Hero />
-      </section>
-      <section className="px-6 lg:px-36 pb-32">
-        <Service />
-      </section>
-      <section>
-        <How />
-      </section>
-    </div>
+  return (
+    <>
+      <div>
+        <Head>
+          <title>MakeWay App Website</title>
+          <meta name="description" content="Developed by JAPECorp" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <section className="h-screen">
+          <Nav sideBar={sideBar} setSideBar={setSideBar} />
+          <Hero />
+        </section>
+        <section className="px-6 lg:px-36 pb-32">
+          <Service />
+        </section>
+        <section>
+          <How />
+        </section>
+      </div>
+      <SideBar sideBar={sideBar} />
+    </>
   );
 }
