62f7854fb67aa6f36297506664aedd95cd087fc5
Eugene Debrah <debraheug@gmail.com>
2022-03-07 07:38:01 +0000

    ...

--- components/HowItWorks/How.jsx
+++ components/HowItWorks/How.jsx
@@ -1,4 +1,4 @@
-import HowComponent from "./HowComponent";
+import Image from "next/image";
 
 function How() {
   const howArray = [
@@ -25,8 +25,23 @@ function How() {
     <div className="md:mt-20">
       <h2 className="text-black text-center">How It Works</h2>
 
-      <div className="text-black">GJHSGKDH</div>
-      <div className="mt-32"></div>
+      <div className="w-1/2 flex items-center">
+        <div className="mt-20">
+          <h4 className="text-blue-500">Solutions</h4>
+          <h3 className="text-black">MakeWay for People</h3>
+          <p className="mt-2 text-gray-400">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Utenim
+            ad minim veniam, quis nostrud exercitation ullamco laborisnisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor
+            inreprehenderit in voluptate velit esse cillum dolore eu fugiat
+          </p>
+        </div>
+      </div>
+
+      <div className="w-full">
+        <div className="h-full w-full relative hidden lg:block">ghsdsdjh</div>
+      </div>
     </div>
   );
 }
