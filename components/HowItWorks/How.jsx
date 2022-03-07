0ad455913e8340e651e4345f80432a2ca53a6f81
Eugene Debrah <debraheug@gmail.com>
2022-03-07 07:00:30 +0000

    How Component

--- components/HowItWorks/How.jsx
+++ components/HowItWorks/How.jsx
@@ -1,4 +1,24 @@
 function How() {
+  const howArray = [
+    {
+      subTitle: "Solution",
+      title: "MakeWay for People",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Utenim ad minim veniam, quis nostrud exercitation ullamco laborisnisi ut aliquip ex ea commodo consequat. Duis aute irure dolor inreprehenderit in voluptate velit esse cillum dolore eu fugiat",
+      imgSrc: "/",
+      id: 1,
+    },
+    {
+      subTitle: "Solution",
+      title: "MakeWay for People",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Utenim ad minim veniam, quis nostrud exercitation ullamco laborisnisi ut aliquip ex ea commodo consequat. Duis aute irure dolor inreprehenderit in voluptate velit esse cillum dolore eu fugiat",
+      imgSrc: "/",
+      id: 2,
+      lgFlexDirection: "g:flex-row-reverse",
+    },
+  ];
+
   return (
     <div className="md:mt-10">
       <h2 className="text-black text-center">How It Works</h2>
