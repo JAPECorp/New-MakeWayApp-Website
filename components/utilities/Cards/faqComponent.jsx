de977b42168ba9c8f20b7b1897975c51b3a74f3f
Eugene Debrah <debraheug@gmail.com>
2022-03-16 09:52:59 +0000

    icon added faq

--- components/utilities/Cards/faqComponent.jsx
+++ components/utilities/Cards/faqComponent.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { IoIosArrowDown } from "react-icons/io";
 
 function FaqComponenet(props) {
   const [expanded, setExpanded] = useState(false);
@@ -6,24 +7,20 @@ function FaqComponenet(props) {
   return (
     <div
       onClick={() => setExpanded(!expanded)}
-      className="flex flex-col p-3 border-b text-gray-light cursor-pointer"
+      className="flex flex-col p-8 border-b text-gray-light cursor-pointer"
     >
       <div className="flex flex-row items-center">
         <p
-          className={`flex-auto hover:text-yellow-500 ${
+          className={`flex-auto hover:text-blue-500 md:hover:text-blue-500 ${
             expanded ? "text-gray-dark font-black" : "font-normal"
           }`}
         >
           {props.title}
         </p>
-        <img
-          className="flex-none w-3"
-          src="/images/icon-arrow-down.svg"
-          alt="arrow down"
-        />
+        <IoIosArrowDown size={25} />
       </div>
       <div
-        className={`transition-max-height duration-700 ease-in-out overflow-hidden ${
+        className={`transition-max-height duration-500 ease-in-out overflow-hidden ${
           expanded ? "max-h-20" : "max-h-0"
         }`}
       >
