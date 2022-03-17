aea1a0bb8871ddbf8285fa37f64786be54fed9ad
Eugene Debrah <debraheug@gmail.com>
2022-03-17 13:52:49 +0000

    404 page

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Head from "next/head";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 
@@ -7,12 +8,17 @@ const NotFound = () => {
 
   useEffect(() => {
     setTimeout(() => {
-      router.push("/");
+      // router.push("/");
     }, 4000);
   }, []);
 
   return (
     <>
+      <Head>
+        <title>MakeWay App Website| 404</title>
+        <meta name="description" content="Developed by JAPECorp" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <div className="h-screen">
         <div className="relative top-makeway-hero-offset-t">
           <div className="">
