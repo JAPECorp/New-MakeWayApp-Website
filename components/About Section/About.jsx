430566a372b10b1372bc4c35cbb5cb03bdbd1d6a
Eugene Debrah <debraheug@gmail.com>
2022-03-20 16:40:52 +0000

    ..

--- components/About Section/About.jsx
+++ components/About Section/About.jsx
@@ -6,10 +6,10 @@ function AboutSection(props) {
       <div className="mt-32">
         <h1 className="text-black text-left text-makeway-md">About Us</h1>
 
-        <h3 className=" text-black mt-5">
+        <p className="text-xl md:text-3xl text-black mt-5">
           <span className="text-blue-600">‘</span>We believe that We can Reduce
           Traffic in our Neighbourhood.<span className="text-blue-600">’</span>
-        </h3>
+        </p>
         <div>
           <Image
             layout="fill"
