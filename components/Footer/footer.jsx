cae84c03107bc042d5aa8c91dcc6cf52d25fd5a3
Eugene Debrah <debraheug@gmail.com>
2022-03-20 08:37:21 +0000

    Icons Added

--- components/Footer/footer.jsx
+++ components/Footer/footer.jsx
@@ -1,5 +1,11 @@
 import Link from "next/link";
 import Image from "next/image";
+import {
+  FaTwitter,
+  FaLinkedinIn,
+  FaFacebookF,
+  FaInstagram,
+} from "react-icons/fa";
 
 function Footer() {
   return (
@@ -21,14 +27,14 @@ function Footer() {
             <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-black">
               Product
             </h2>
-            <ul className="text-gray-600 dark:text-gray-400">
+            <ul className="text-gray-600">
               <li className="mb-4">
-                <a href="https://flowbite.com" className="hover:underline">
+                <a href="" className="hover:text-blue-500">
                   About Us
                 </a>
               </li>
               <li>
-                <a href="https://tailwindcss.com/" className="hover:underline">
+                <a href="" className="hover:text-blue-500">
                   FAQ
                 </a>
               </li>
@@ -39,14 +45,14 @@ function Footer() {
             <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-black">
               Information
             </h2>
-            <ul className="text-gray-600 dark:text-gray-400">
+            <ul className="text-gray-600">
               <li className="mb-4">
-                <a href="#" className="hover:underline">
+                <a href="#" className="hover:text-blue-500">
                   Privacy Policy
                 </a>
               </li>
               <li>
-                <a href="#" className="hover:underline">
+                <a href="#" className="hover:text-blue-500">
                   Support
                 </a>
               </li>
@@ -54,9 +60,9 @@ function Footer() {
           </div>
         </div>
       </div>
-      <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+      <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-200 lg:my-8" />
       <div className="sm:flex sm:items-center sm:justify-between">
-        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        <span className="text-sm text-black sm:text-center">
           © 2022{" "}
           <a href="" className="hover:underline">
             MakeWay
@@ -64,35 +70,18 @@ function Footer() {
           . All Rights Reserved.
         </span>
         <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
-          <a
-            href="#"
-            className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
-          ></a>
-          <a
-            href="#"
-            className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
-          ></a>
-          <a
-            href="#"
-            className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
-          >
-            <svg
-              className="w-5 h-5"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-              aria-hidden="true"
-            >
-              <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
-            </svg>
+          <a href="#" className="text-blue-600 hover:text-gray-900 ">
+            <FaInstagram />
           </a>
-          <a
-            href="#"
-            className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
-          ></a>
-          <a
-            href="#"
-            className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
-          ></a>
+          <a href="#" className="text-blue-600 hover:text-gray-900 ">
+            <FaFacebookF />
+          </a>
+          <a href="#" className="text-blue-600 hover:text-gray-900 ">
+            <FaTwitter />
+          </a>
+          <a href="#" className="text-blue-600 hover:text-gray-900 ">
+            <FaLinkedinIn />
+          </a>
         </div>
       </div>
     </footer>
