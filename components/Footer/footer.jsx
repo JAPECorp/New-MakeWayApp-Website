0a882a8775379e47e94cbd8e2d1506cb01397d41
Eugene Debrah <debraheug@gmail.com>
2022-03-20 08:07:31 +0000

    Rechange of footer

--- components/Footer/footer.jsx
+++ components/Footer/footer.jsx
@@ -3,46 +3,121 @@ import Image from "next/image";
 
 function Footer() {
   return (
-    <footer className="px-10 mt-10">
-      <div className="sm:flex items-center sm:justify-between">
-        <Link href="/" className="flex items-center mb-4 sm:mb-0" passHref>
-          <img
-            src="/Logo2.png"
-            className="mr-6 h-12 content-center"
-            alt="Logo"
-          />
-        </Link>
-        <ul className="flex mb-10 text-sm text-black sm:mb-0 ">
-          <li>
-            <Link href="/about">
-              <a className="mr-4 hover:text-blue-500 ">About</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              <a className="mr-4 hover:text-blue-500">FAQ</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/about">
-              <a className="mr-4 hover:text-blue-500">Support</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/about">
-              <a className="mr-4 hover:text-blue-500">Privacy Policy</a>
-            </Link>
-          </li>
-        </ul>
+    <footer className="p-4 bg-white sm:p-6 dark:bg-gray-800">
+      <div className="md:flex md:justify-between">
+        <div className="mb-6 md:mb-0">
+          <a href="https://flowbite.com" className="flex items-center">
+            <img
+              src="/docs/images/logo.svg"
+              className="mr-3 h-8"
+              alt="FlowBite Logo"
+            />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              Flowbite
+            </span>
+          </a>
+        </div>
+        <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+          <div>
+            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+              Resources
+            </h2>
+            <ul className="text-gray-600 dark:text-gray-400">
+              <li className="mb-4">
+                <a href="https://flowbite.com" className="hover:underline">
+                  Flowbite
+                </a>
+              </li>
+              <li>
+                <a href="https://tailwindcss.com/" className="hover:underline">
+                  Tailwind CSS
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+              Follow us
+            </h2>
+            <ul className="text-gray-600 dark:text-gray-400">
+              <li className="mb-4">
+                <a
+                  href="https://github.com/themesberg/flowbite"
+                  className="hover:underline "
+                >
+                  Github
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://discord.gg/4eeurUVvTy"
+                  className="hover:underline"
+                >
+                  Discord
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+              Legal
+            </h2>
+            <ul className="text-gray-600 dark:text-gray-400">
+              <li className="mb-4">
+                <a href="#" className="hover:underline">
+                  Privacy Policy
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline">
+                  Terms &amp; Conditions
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
-
-      <span className="block text-base text-black text-center dark:text-black mt-4">
-        © 2022{" "}
-        <a href="https://makewayapp.com" className="hover:underline">
-          MakeWay
-        </a>
-        . All Rights Reserved.
-      </span>
+      <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+      <div className="sm:flex sm:items-center sm:justify-between">
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2022{" "}
+          <a href="https://flowbite.com" className="hover:underline">
+            Flowbite™
+          </a>
+          . All Rights Reserved.
+        </span>
+        <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
+          <a
+            href="#"
+            className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+          ></a>
+          <a
+            href="#"
+            className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+          ></a>
+          <a
+            href="#"
+            className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+          >
+            <svg
+              className="w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+              aria-hidden="true"
+            >
+              <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+            </svg>
+          </a>
+          <a
+            href="#"
+            className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+          ></a>
+          <a
+            href="#"
+            className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+          ></a>
+        </div>
+      </div>
     </footer>
   );
 }
