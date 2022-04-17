6a5264bf922eb989d05f7b678007e61d7c0cfcfa
Eugene Debrah <debraheug@gmail.com>
2022-04-17 13:22:13 +0000

    Icons Added

--- components/utilities/Cards/HowComponenets.jsx
+++ components/utilities/Cards/HowComponenets.jsx
@@ -19,12 +19,19 @@ function HowComponent(props) {
               <li className="mb-10 ml-6">
                 <span className="ring">
                   <svg
-                    className="w-3 h-3 text-blue-600 dark:text-blue-400"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
+                    id="Add_User"
+                    data-name="Add User"
                     xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="18"
+                    viewBox="0 0 20 19"
                   >
-                    <path d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"></path>
+                    <path
+                      id="Add_User-2"
+                      data-name="Add User"
+                      d="M0,15.746c0-2.585,3.454-3.231,7.5-3.231,4.067,0,7.5.669,7.5,3.253S11.545,19,7.5,19C3.434,19,0,18.329,0,15.746Zm16.1-5.658V8.911H14.9a.912.912,0,0,1,0-1.823h1.2V5.911a.9.9,0,1,1,1.8,0V7.088h1.2a.912.912,0,0,1,0,1.823H17.9v1.177a.9.9,0,1,1-1.8,0ZM2.537,5.027a4.963,4.963,0,1,1,9.925,0,4.963,4.963,0,1,1-9.925,0Z"
+                      fill="#1E5CDE"
+                    />
                   </svg>
                 </span>
                 <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900">
@@ -40,12 +47,20 @@ function HowComponent(props) {
               <li className="mb-10 ml-6">
                 <span className="ring">
                   <svg
-                    className="w-3 h-3 text-blue-600 dark:text-blue-400"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
                     xmlns="http://www.w3.org/2000/svg"
+                    width="14"
+                    height="18"
+                    viewBox="0 0 20 20"
                   >
-                    <path d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"></path>
+                    <g id="Plus" transform="translate(0 0)">
+                      <path
+                        id="Plus-2"
+                        data-name="Plus"
+                        d="M14.67,20H5.33a5.349,5.349,0,0,1-3.944-1.394A5.356,5.356,0,0,1,0,14.67V5.33A5.358,5.358,0,0,1,1.386,1.386,5.358,5.358,0,0,1,5.33,0h9.33a5.372,5.372,0,0,1,3.945,1.386A5.345,5.345,0,0,1,20,5.33v9.34C20,18.057,18.057,20,14.67,20ZM6.33,9.16a.819.819,0,0,0-.83.83.839.839,0,0,0,.83.84H9.16V13.66a.83.83,0,1,0,1.66,0V10.83h2.84a.835.835,0,0,0,0-1.669H10.82V6.34a.83.83,0,1,0-1.66,0V9.16Z"
+                        transform="translate(0 0)"
+                        fill="#1E5CDE"
+                      />
+                    </g>
                   </svg>
                 </span>
                 <h3 className="mb-1 text-lg font-semibold text-gray-900">
@@ -61,12 +76,18 @@ function HowComponent(props) {
               <li className="mb-10 ml-6">
                 <span className="ring">
                   <svg
-                    className="w-3 h-3 text-blue-600 dark:text-blue-400"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
+                    id="Send"
                     xmlns="http://www.w3.org/2000/svg"
+                    width="14"
+                    height="18"
+                    viewBox="0 0 20 20"
                   >
-                    <path d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"></path>
+                    <path
+                      id="Send-2"
+                      data-name="Send"
+                      d="M19.435.582A1.933,1.933,0,0,0,17.5.079L1.408,4.76A1.919,1.919,0,0,0,.024,6.281a2.253,2.253,0,0,0,1,2.1L6.06,11.477a1.3,1.3,0,0,0,1.61-.193l5.763-5.8a.734.734,0,0,1,1.06,0,.763.763,0,0,1,0,1.067l-5.773,5.8a1.324,1.324,0,0,0-.193,1.619L11.6,19.054A1.91,1.91,0,0,0,13.263,20a2.078,2.078,0,0,0,.25-.01A1.95,1.95,0,0,0,15.144,18.6L19.916,2.525a1.964,1.964,0,0,0-.48-1.943"
+                      fill="#1E5CDE"
+                    />
                   </svg>
                 </span>
                 <h3 className="mb-1 text-lg font-semibold text-gray-900 d">
