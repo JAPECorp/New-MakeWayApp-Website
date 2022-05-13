5dfe74968a1a5ff387e581f761b5135f88ba9167
Eugene Debrah <debraheug@gmail.com>
2022-05-13 06:52:42 -1200

    Instruction Card

--- components/utilities/Cards/InstructionsCard.jsx
+++ components/utilities/Cards/InstructionsCard.jsx
@@ -5,8 +5,8 @@ function InstructionsCard(props) {
     <motion.div whileHover={{ scale: 1.01 }} className={props.className}>
       <div className="text-start mt-4">
         <h4 className="text-blue-700 text-2xl font-bold">{props.number}</h4>
-        <h4 className=" mt-4 text-white leading-tight font-semibold">{props.title}</h4>
-        <p className="mt-7 text-lg text-blue-900 ">{props.content}</p>
+        <h4 className=" mt-4 text-blue-900 leading-tight font-semibold">{props.title}</h4>
+        <p className="mt-7 text-lg font-normal text-blue-700 ">{props.content}</p>
       </div>
     </motion.div>
   );
