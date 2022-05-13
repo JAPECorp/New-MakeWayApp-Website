47c80c381064a522bd6251244b5a2d1fc67067dd
Eugene Debrah <debraheug@gmail.com>
2022-05-13 01:49:07 -1100

    Instruction Card

--- components/utilities/Cards/InstructionsCard.jsx
+++ components/utilities/Cards/InstructionsCard.jsx
@@ -3,7 +3,7 @@ const { motion } = require("framer-motion");
 function InstructionsCard(props) {
   return (
     <motion.div whileHover={{ scale: 1.01 }} className={props.className}>
-      <div className="text-center mt-4">
+      <div className="text-start mt-4">
         <h4 className="text-blue-600">{props.number}</h4>
         <h4 className=" mt-4 text-white">{props.title}</h4>
         <p className="mt-7 text-ijikod-sm">{props.content}</p>
