f2da2f5846d97ec02775ac8e15e955fe61879b2c
Eugene Debrah <debraheug@gmail.com>
2022-05-13 12:50:47 -1200

    InstructionCard changed to Guide Card

---
+++ components/utilities/Cards/GuideCard.jsx
@@ -0,0 +1,15 @@
+const { motion } = require("framer-motion");
+
+function InstructionsCard(props) {
+  return (
+    <motion.div whileHover={{ scale: 1.01 }} className={props.className}>
+      <div className="text-start mt-4">
+        <h4 className="text-blue-700 text-2xl font-bold">{props.number}</h4>
+        <h4 className=" mt-4 text-blue-900 leading-tight font-semibold">{props.title}</h4>
+        <p className="mt-7 text-lg font-normal text-blue-700 ">{props.content}</p>
+      </div>
+    </motion.div>
+  );
+}
+
+export default InstructionsCard;
