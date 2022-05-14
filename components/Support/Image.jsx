360b791d19947ac64816f69204337344d54b373d
Eugene Debrah <debraheug@gmail.com>
2022-05-13 14:09:13 -1200

    support image

---
+++ components/Support/Image.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable @next/next/no-img-element */
+const SupportImage = () => {
+    return ( <div>
+    <img className="image" src="/support.jpg" alt="/" style={{maxWidth: "100%"}} />
+      
+    </div> );
+}
+ 
+export default SupportImage;
